89fda3f9e0bda3ad6c3076564b4cc6ce454dad08
iianTP <iantpimentel@gmail.com>
2025-04-12 12:31:22 -0300

    teste plataformer
    nível de teste e wall jumping

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -11,6 +11,10 @@ public class PlayerScript : MonoBehaviour
     public InputActionReference movement, interaction, attack;
 
     public GameObject sword;
+    public Transform wallTransform;
+    public Transform groundTransform;
+    public LayerMask wallLayer;
+    public LayerMask groundLayer;
 
     public float speed;
     public string direction;
@@ -24,27 +28,37 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        playerMovement();
-        updateDirection();
+        PlayerMovement();
+        UpdateDirection();
+        WallSlide();
     }
 
 
-    void playerMovement()
+    void PlayerMovement()
     {
 
-        rb.linearVelocity = movement.action.ReadValue<Vector2>() * speed;
+        //rb.linearVelocity = movement.action.ReadValue<Vector2>() * speed;
+
+        rb.linearVelocityX = movement.action.ReadValue<Vector2>().x * speed;
+        if (Input.GetKeyDown(KeyCode.Space) && rb.linearVelocityY == 0)
+        {
+            rb.linearVelocityY = 15;
+        }
+
+
 
         if (GameObject.Find("Sword(Clone)")) { rb.linearVelocity *= 0; }
 
     }
 
-    void updateDirection()
+    void UpdateDirection()
     {
-        
 
         if (rb.linearVelocityX != 0)
         {
+
             direction = (rb.linearVelocityX < 0) ? "left" : "right";
+            transform.localScale = (rb.linearVelocityX < 0) ? new Vector3(-0.5f, 0.5f, 0.5f) : new Vector3(0.5f, 0.5f, 0.5f);
         }
 
         if (rb.linearVelocityY != 0)
@@ -53,27 +67,45 @@ public class PlayerScript : MonoBehaviour
         }
     }
 
-    private void action(InputAction.CallbackContext context)
+    bool IsOnGround()
+    {
+        return Physics2D.OverlapCircle(groundTransform.position, 0.01f, groundLayer);
+    }
+
+    bool IsOnWall()
+    {
+        return Physics2D.OverlapCircle(wallTransform.position, 0.01f, wallLayer);
+    }
+
+    void WallSlide()
+    {
+        if (IsOnWall() && !IsOnGround())
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocityX, Mathf.Clamp(rb.linearVelocityY, -2f, float.MaxValue));
+        }
+    }
+
+    private void Action(InputAction.CallbackContext context)
     {
         Instantiate(sword);
     }
 
-    private void t(InputAction.CallbackContext context)
+    private void T(InputAction.CallbackContext context)
     {
-        Debug.Log("asdiugh");
+
     }
 
     private void OnEnable()
     {
-        interaction.action.performed += t;
-        attack.action.performed += action;
+        interaction.action.performed += T;
+        attack.action.performed += Action;
         
     }
 
     private void OnDisable()
     {
-        interaction.action.performed -= t;
-        attack.action.performed -= action;
+        interaction.action.performed -= T;
+        attack.action.performed -= Action;
     }
 
     
